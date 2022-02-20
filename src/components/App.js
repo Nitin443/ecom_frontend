3a3b15783a04362dd2e217fbc8439e196b20386f
Nitin443 <imnitin55@gmail.com>
2022-02-20 10:39:22 +0530

    add routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,27 @@
-import './App.css';
+import Signup from "./user/Signup";
+import Login from "./user/Login";
+import Home from "./core/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
       
+    <Router>
+
+    <Routes>
+
+    <Route exact path='/' element={<Home />} />
+
+    <Route exact path='/signup' element={<Signup />} />
+
+    <Route exact path='/login' element={<Login />} />
+
+    </Routes>
+
+    </Router>
+
     </div>
   );
 }
