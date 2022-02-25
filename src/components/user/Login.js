4705b1927ecb0b79e96a0809aaf3161e2705b714
Nitin443 <imnitin55@gmail.com>
2022-02-25 10:56:53 +0530

    connect signup api to backend

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -1,17 +1,55 @@
-import React from "react";
+import React, {useState} from "react";
 import Layout from "../core/Layout";
 import Menu from "../core/Menu";
 
 function Login() {
+
+    const[loginVal, setLoginVal] = useState({
+       email: '',
+       password: '',
+       error: '',
+       success: false
+    })
+
+      function handleChange(name){
+          return(
+              (event) => {
+                setLoginVal({...loginVal, error: false, [name]: event.target.value});
+              }
+          );
+      }
+
+    function loginHandler() {
+        return (
+            <form>
+
+                <div className="form-group">
+                    <label className="text-muted">Email</label>
+                    <input onChange={handleChange('email')} type="email" className="form-control" />
+                </div>
+
+                <div className="form-group">
+                    <label className="text-muted">Password</label>
+                    <input onChange={handleChange('password')} type="password" className="form-control" />
+                </div>
+
+                <button className="btn btn-primary">Login</button>
+
+            </form>
+        );
+    }
+
     return (
         <div className='login'>
 
-         <Menu />
+            <Menu />
 
-         <Layout 
-             title="login page"
-             description="welcome at login page"
-         />
+            <Layout
+                title="Login"
+                description="Login to Your Account"
+                className="container col-md-6 offset-md-6" children={loginHandler()}
+            
+            ></Layout>
         </div>
     );
 }
--- src/components/user/Signup.js
+++ src/components/user/Signup.js
@@ -7,21 +7,66 @@ import Menu from "../core/Menu";
 function Signup() {
 
     const [values, setValues] = useState({
-        name: '',
+        names: '',
         email: '',
         password: '',
         error: '',
         success: false
     })
 
-    // function handleChange(name){
-    //     (event) => {
-    //         setValues({...values, error: false, [name]: event.target.value});
-    //     }
-    // }
+    function handleChange(name) {
+        return (
+            (event) => {
+                setValues({ ...values, error: false, [name]: event.target.value });
+            }
+        )
+    }
 
-    const handleChange = name => event => {
-        setValues({ ...values, error: false, [name]: event.target.value });
+    // const handleChange = (name) => (event) => {
+    //     setValues({ ...values, error: false, [name]: event.target.value });
+    // };
+
+    const { name, email, password } = values;
+
+    // const signup = (user) => {
+    //   // console.log(user);
+    //   fetch('http://localhost:8000/signup', {
+    //       method: "POST",
+    //       headers: {
+    //           Accept: "application/json",
+    //           "Content-Type": "application/json"
+    //       },
+    //       body: JSON.stringify(user)
+    //   })
+    //   .then(response => {
+    //       return response.json();
+    //   })
+    //   .catch(err => {
+    //       console.log(err)
+    //   });
+    // };
+
+  const signup =  async (user) => {
+        try {
+            const data = await fetch('http://localhost:8000/signup', {
+                method: "POST",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(user)
+            });
+
+            return data.json();
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const clickSignup = event => {
+        event.preventDefault();
+        signup({ name, email, password });
     };
 
     const signupForm = () => {
@@ -43,7 +88,7 @@ function Signup() {
                     <input onChange={handleChange('password')} type="password" className="form-control" />
                 </div>
 
-                <button className="btn btn-primary">SignUp</button>
+                <button onClick={clickSignup} className="btn btn-primary">SignUp</button>
 
             </form>
         );
@@ -55,8 +100,8 @@ function Signup() {
 
             <Menu />
 
-            <Layout title="Signup" description="Signup To Our App" className="container col-md-6 offset-md-6">
-                {signupForm()}
+            <Layout title="Signup" description="Signup To Our App" className="container col-md-6 offset-md-6" children={signupForm()}>
+                {/* {signupForm()} */}
             </Layout>
 
         </div>
