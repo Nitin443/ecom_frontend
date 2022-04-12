2e4dfe1ae6aca3937c87b123ffbff1ee922c8b08
Nitin443 <imnitin55@gmail.com>
2022-04-12 09:47:49 +0530

    add style in manage products

--- src/components/admin/ManageProducts.js
+++ src/components/admin/ManageProducts.js
@@ -3,6 +3,7 @@ import Layout from '../core/Layout';
 import Menu from '../core/Menu';
 import { Link } from 'react-router-dom';
 import { getProducts, deleteProduct } from './adminApi';
+import './ManageProducts.css'
 
 const ManageProducts = () => {
 const[products, setProducts] = useState([]);
@@ -40,20 +41,20 @@ useEffect(() => {
                 description="Here You Can Manage Your Products "
                 className="container-fluid"
             />
-            <div className='container'>
+            <div className='container topDiv'>
                 <div className=''>
-                <h2 className='text-center'>
+                <h2 className='text-center h1Color'>
                    Total  {products.length} Products
                 </h2>
                     <ul className='list-group-item'>
                         {products.map((p, i) => {
                             return(
-                                <li key={i} className='list-group-item'>
-                                    <strong>{p.name}</strong>
-                                    <Link to={`/admin/product/update/${p._id}`}>
-                                        <span className='badge'>Update</span>
+                                <li key={i} className='list-group-item '>
+                                    <strong className='inDiv'>{p.name}</strong>
+                                    <Link to={`/admin/product/update/${p._id}`} >
+                                        <span className='badge updateBtn'>Update</span>
                                     </Link>
-                                    <span onClick={() => deletePro(p._id)} className='badge'>
+                                    <span onClick={() => deletePro(p._id)} className='badge deleteBtn'>
                                         Delete
                                     </span>
                                 </li>
